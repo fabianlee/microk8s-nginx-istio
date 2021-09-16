a00f44e0ac0a7e155249a21b88a6ef2edaa65958
Fabian Lee <flee999@hotmail.com>
2021-09-15 23:05:53 -0400

    test of endpoints

---
+++ test-ngingx-endpoints.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for h in microk8s.local microk8s-secondary.local; do
+  nslookup -timeout=1 $h
+  if [ $? -ne 0 ]; then
+    echo "WARN are you sure $h is DNS resolvable? did you add it to /etc/hosts?"
+    #exit 3
+  fi
+done
+
+curlopt="-k --fail --connect-timeout 3 --retry 0"
+while [ 1==1 ]; do 
+  timestamp=$(date +"%D %T")
+  echo ""
+  echo "$timestamp"
+  curl $curlopt https://microk8s.local/myhello/
+  curl $curlopt https://microk8s-secondary.local/myhello2/
+  sleep 4
+done
